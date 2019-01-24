879e763a9bfdcee857706d07500b6416f05502c0
Drainyyyy <Drainyyyy@users.noreply.github.com>
2019-01-24 16:40:40 +0100

    Removed redunant to

--- utils/presence.py
+++ utils/presence.py
@@ -10,12 +10,12 @@ class Presence:
     def __init__(self, bot):
         self.bot = bot
         self.presences = [
-            (discord.Status.dnd, discord.ActivityType.watching, f"over {len(self.bot.guilds)} servers!"),
-            (discord.Status.dnd, discord.ActivityType.listening, f"{config.prefix}help | {config.prefix}commands"),
-            (discord.Status.dnd, discord.ActivityType.watching, f"over {len(self.bot.guilds)} servers with a total of {len(self.bot.users)} users"),
-            (discord.Status.dnd, discord.ActivityType.playing, "with shitcode"),
-            (discord.Status.dnd, discord.ActivityType.listening, "to you."),
-            (discord.Status.dnd, discord.ActivityType.playing, "mention me!"),
+            (discord.Status.dnd, discord.ActivityType.watching, f" over {len(self.bot.guilds)} servers!"),
+            (discord.Status.dnd, discord.ActivityType.listening, f" {config.prefix}help | {config.prefix}commands"),
+            (discord.Status.dnd, discord.ActivityType.watching, f" over {len(self.bot.guilds)} servers with a total of {len(self.bot.users)} users"),
+            (discord.Status.dnd, discord.ActivityType.playing, " with shitcode"),
+            (discord.Status.dnd, discord.ActivityType.listening, " you."),
+            (discord.Status.dnd, discord.ActivityType.playing, " mention me!"),
             ]
 
     async def start_status(self):
