8ae0fbe5284ec0f03a75ebe1b4d108afb9937657
Drainyyyy <Drainyyyy@users.noreply.github.com>
2019-01-24 16:42:08 +0100

    Added the ID to the error embed

--- utils/message/embeds.py
+++ utils/message/embeds.py
@@ -7,7 +7,7 @@ def error(message, exception, ctx):
     embed = Embed(title="Error", color=colors.error, description=message)
     embed.add_field(name="Command", value=ctx.message.content, inline=False)
     embed.add_field(name="Exception", value=f"```{exception}```", inline=False)
-    embed.set_footer(text=f"Triggered by {ctx.author}", icon_url=ctx.author.avatar_url)
+    embed.set_footer(text=f"Triggered by {ctx.author} ({ctx.author.id})", icon_url=ctx.author.avatar_url)
     return embed
 
 
