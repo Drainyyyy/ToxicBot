7e6be9cd2f1536a2e97df39e437caa9dff26a81b
Drainyyyy <videomoritz@gmail.com>
2019-06-11 21:47:25 +0200

    Clarified descriptions of config parameters

--- config.example.py
+++ config.example.py
@@ -3,27 +3,27 @@ prefix = "Prefix"
 bot_token = "Token"
 
 #   Database
-db_user = "User"
-db_host = "Host"
-db_password = "Password"
-db_name = "Database Name"
+db_user = "Database user"
+db_host = "Database host"
+db_password = "Database password"
+db_name = "Database name"
 
 #   Discord Bot List
-dbl_token = "Token for DiscordBotsOrgAPI"
+dbl_token = "Token for the discordbots.org api"
 
 #   GitHub
 github_access_token = "Access token for github"
-repository_url = "Url of the bot repo"
+repository_url = "Url of the bot repository"
 repository_name = "Repository name"
 
 #   Misc
-err_wh_url = "Webhook url for the errors"
-notif_wh_url = "Notification webhook url"
+err_wh_url = "Webhook url for the error messages"
+notif_wh_url = "Webhook url for the notifications"
 vote = "Vote link for discordbots.org"
-invite = "Invite link"
+invite = "Invite link for the bot"
 website = "Website url"
-discord_server = "Support server link"
+discord_server = "Link to support discord server"
 patreon = "Link to patreon"
-owner_id = 12345 #Owner ID (As int!)
+owner_id = 12345  # as integer!
 bot_role = "Bot permissions role"
 standard_mod_message = "The message shown when no reason is submitted by banning/kicking someone."
