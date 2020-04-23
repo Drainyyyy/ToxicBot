5ee98ffbc3340fc18a5cea77598d076ffcf950cc
Drainyyy <joergmoritz.reinhardt@googlemail.com>
2020-04-23 19:32:50 +0200

    Fixed for running on newer discord.py version

--- cogs/utils/dbl.py
+++ cogs/utils/dbl.py
@@ -16,7 +16,7 @@ class DiscordBotsOrgAPI(commands.Cog, name="DiscordBotsOrgAPI"):
     def __init__(self, bot):
         self.bot = bot
         self.token = config.dbl_token
-        self.dblpy = dbl.Client(self.bot, self.token)
+        self.dblpy = dbl.DBLClient(self.bot, self.token)
         self.bot.loop.create_task(self.update_stats())
 
     async def update_stats(self):
